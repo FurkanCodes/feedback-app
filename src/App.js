6e16d26219008b166ac8a833c08c09569f25defa
Furkan <turkogluvelifurkan@gmail.com>
2022-10-12 00:44:48 +0300

    fourth

--- src/App.js
+++ src/App.js
@@ -2,15 +2,48 @@ import { useState } from "react";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import { v4 as uuidv4 } from "uuid";
+import About from "./pages/About";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AboutIcon from "./components/AboutIcon";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
   return (
     <>
-      <Header />
-      <div className="container">
-        <FeedbackList feedback={feedback} />
-      </div>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                </>
+              }
+            ></Route>
+
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <AboutIcon />
+        </div>
+      </Router>
     </>
   );
 }
--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,7 +1,7 @@
 const FeedbackData = [
   {
     id: 1,
-    rating: 10,
+    rating: 5,
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Vitae suscipit tellus mauris a. Id donec ultrices tincidunt arcu non sodales neque sodales. Proin sed libero enim sed faucibus turpis in.",
   },
   {
